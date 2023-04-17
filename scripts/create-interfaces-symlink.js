9d92dc899cf50953ce525662833edc2011834b7b
kolserdav <serega12101983@gmail.com>
2023-04-17 13:59:32 +0700

    windows scripts

--- scripts/create-interfaces-symlink.js
+++ scripts/create-interfaces-symlink.js
@@ -1,18 +1,27 @@
 // @ts-check
-const { existsSync, readlinkSync, symlink } = require('fs');
+const { existsSync, readFileSync, writeFileSync, readlinkSync, symlink } = require('fs');
 const { resolve } = require('path');
+const os = require('os');
+
+const isWin = os.platform() === 'win32';
 
 const destPath = resolve(__dirname, '../packages/server/src/types/interfaces.ts');
 const srcPath = resolve(__dirname, '../packages/app/types/interfaces.ts');
 
-if (existsSync(destPath)) {
-  console.info('Symlink created:', destPath);
+if (isWin) {
+  const data = readFileSync(srcPath);
+  writeFileSync(destPath, data);
+  console.info('Interface copied', destPath);
 } else {
-  symlink(srcPath, destPath, 'file', (err) => {
-    if (err) {
-      console.error('Error create symlink', err);
-      return;
-    }
+  if (existsSync(destPath)) {
     console.info('Symlink created:', destPath);
-  });
+  } else {
+    symlink(srcPath, destPath, 'file', (err) => {
+      if (err) {
+        console.error('Error create symlink', err);
+        return;
+      }
+      console.info('Symlink created:', destPath);
+    });
+  }
 }
