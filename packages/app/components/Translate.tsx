9ecc55afbcfd2089fdbd47fbc00b1efd059be554
kolserdav <serega12101983@gmail.com>
2023-05-18 17:32:25 +0700

    revert dev state

--- packages/app/components/Translate.tsx
+++ packages/app/components/Translate.tsx
@@ -236,7 +236,7 @@ function Translate({
               {undo ? <UndoIcon color={theme.text} /> : <CloseIcon color={theme.text} />}
             </IconButton>
           </div>
-          {!allowRecogn && (
+          {allowRecogn && (
             <div className={s.micro_button} title={locale.startRecognize}>
               <IconButton
                 disabled={load}
