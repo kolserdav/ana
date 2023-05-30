afe4297e95956dc76791064962c9623646fa7b3b
kolserdav <serega12101983@gmail.com>
2023-05-30 11:28:49 +0700

    set endglish by default for android

--- android/app/src/main/java/com/kolserdav/ana/MainActivity.java
+++ android/app/src/main/java/com/kolserdav/ana/MainActivity.java
@@ -62,7 +62,7 @@ public class MainActivity extends Activity {
             }
         });
 
-        mWebView.loadUrl("https://uyem.ru/");
+        mWebView.loadUrl("https://uyem.ru/en");
 
         this.setContentView(mWebView);
 
