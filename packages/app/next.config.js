a0021d7e7707839e32e577a46b94e4bca55927fd
kolserdav <serega12101983@gmail.com>
2023-07-02 03:48:48 +0700

    remove extra

--- packages/app/next.config.js
+++ packages/app/next.config.js
@@ -10,23 +10,6 @@ const nextConfig = {
     defaultLocale: 'en',
     localeDetection: true,
   },
-  async headers() {
-    return [
-      {
-        source: '/api/check',
-        headers: [
-          { key: 'Access-Control-Allow-Credentials', value: 'true' },
-          { key: 'Access-Control-Allow-Origin', value: 'test.uyem.ru' },
-          { key: 'Access-Control-Allow-Methods', value: 'GET' },
-          {
-            key: 'Access-Control-Allow-Headers',
-            value:
-              'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version',
-          },
-        ],
-      },
-    ];
-  },
 };
 
 module.exports = nextConfig;
