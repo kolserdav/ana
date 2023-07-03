7ed4e4ccb975ad94b84a115381de060796c5c497
kolserdav <serega12101983@gmail.com>
2023-07-03 14:27:46 +0700

    git-check load branch from .env file

--- scripts/sh/update.sh
+++ scripts/sh/update.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 PROJECT_ROOT=/usr/local/share/applications/ana-test
 NODE_PATH=/home/nouser/.nvm/versions/node/v18.16.0/bin/node
-GIT_UPDATE_SCRIPT='scripts/git-check.js -e anaserver-test,ana-test -p packages/server,packages/app -b develop'
+GIT_UPDATE_SCRIPT='scripts/git-check.js -e anaserver-test,ana-test -p packages/server,packages/app'
 
 source /home/nouser/.bashrc
 
