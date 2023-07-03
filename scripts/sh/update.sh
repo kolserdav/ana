abf6f39ecca6dc98d10da27d4b7cfb9d4e1a1d6d
kolserdav <serega12101983@gmail.com>
2023-07-03 14:49:11 +0700

    git check provide branch

--- scripts/sh/update.sh
+++ scripts/sh/update.sh
@@ -1,7 +1,17 @@
 #!/usr/bin/env bash
+BRANCH=''
+index=0
+for i in $@;
+do
+    if [ $index -eq 0 ]; then
+      BRANCH=$i
+    fi
+    index=$((index+1))
+done
+
 PROJECT_ROOT=/usr/local/share/applications/ana-test
 NODE_PATH=/home/nouser/.nvm/versions/node/v18.16.0/bin/node
-GIT_UPDATE_SCRIPT='scripts/git-check.js -e anaserver-test,ana-test -p packages/server,packages/app'
+GIT_UPDATE_SCRIPT='scripts/git-check.js -e anaserver-test,ana-test -p packages/server,packages/app -b $BRANCH'
 
 source /home/nouser/.bashrc
 
