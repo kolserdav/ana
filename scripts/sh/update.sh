cbb234abee13634212a87a299dbe512c3996e0c9
kolserdav <serega12101983@gmail.com>
2023-07-03 16:49:54 +0700

    fixed git

--- scripts/sh/update.sh
+++ scripts/sh/update.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 PROJECT_ROOT=/usr/local/share/applications/ana
 NODE_PATH=/home/nouser/.nvm/versions/node/v18.16.0/bin/node
-GIT_UPDATE_SCRIPT='scripts/git-check.js -e anaserver-test,ana-test -p packages/server,packages/app -b master'
+GIT_UPDATE_SCRIPT='scripts/git-check.js -e anaserver,ana -p packages/server,packages/app -b master'
 
 source /home/nouser/.bashrc
 
