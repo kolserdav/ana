e2735cadb6ea87348830f390a72afbf7ed352c01
kolserdav <serega12101983@gmail.com>
2023-07-15 03:35:58 +0700

    connect to web socket

--- android/app/src/main/java/com/kolserdav/ana/DisplayNotification.java
+++ android/app/src/main/java/com/kolserdav/ana/DisplayNotification.java
@@ -11,6 +11,17 @@ import androidx.annotation.Nullable;
 import androidx.core.app.NotificationCompat;
 import androidx.core.app.NotificationManagerCompat;
 
+import com.neovisionaries.ws.client.HostnameUnverifiedException;
+import com.neovisionaries.ws.client.OpeningHandshakeException;
+import com.neovisionaries.ws.client.WebSocket;
+import com.neovisionaries.ws.client.WebSocketAdapter;
+import com.neovisionaries.ws.client.WebSocketException;
+import com.neovisionaries.ws.client.WebSocketFactory;
+
+import java.io.IOException;
+import java.util.List;
+import java.util.Map;
+
 public class DisplayNotification extends Service {
 
     private final String TAG = "DisplayNotification";
@@ -52,6 +63,49 @@ public class DisplayNotification extends Service {
                 // TODO listen notifications
                 while (url == null) {}
                 Log.d(TAG, "Notifications service is running: " + wsAddress + ", " + url);
+                WebSocket ws = null;
+                try {
+                    ws = new WebSocketFactory().createSocket(wsAddress);
+                } catch (IOException e) {
+                    Log.e(TAG, "Error connect to websocket: " + e.getMessage());
+                }
+                if (ws != null) {
+                    ws.addListener(new WebSocketAdapter() {
+                        @Override
+                        public void onTextMessage(WebSocket websocket, String message) throws Exception {
+                           Log.d(TAG, "WebSocket on message: " + message);
+                        }
+
+                        @Override
+                        public void onConnected(WebSocket websocket, Map<String, List<String>> headers) throws Exception {
+                            super.onConnected(websocket, headers);
+                            Log.d(TAG, "WebSocket connected: " + headers);
+                        }
+
+                        @Override
+                        public void onConnectError(WebSocket websocket, WebSocketException exception) throws Exception {
+                            super.onConnectError(websocket, exception);
+                            Log.d(TAG, "WebSocket connect error: " + exception.getMessage());
+                        }
+                    });
+
+                    try
+                    {
+                        ws.connect();
+                    }
+                    catch (OpeningHandshakeException e)
+                    {
+                        Log.e(TAG, "OpeningHandshakeException: " + e.getMessage());
+                    }
+                    catch (HostnameUnverifiedException e)
+                    {
+                        Log.e(TAG, "HostnameUnverifiedException: " + e.getMessage());
+                    }
+                    catch (WebSocketException e)
+                    {
+                        Log.e(TAG, "Failed to establish a WebSocket connection: " + e.getMessage());
+                    }
+                }
             }
         };
         thread.start();
