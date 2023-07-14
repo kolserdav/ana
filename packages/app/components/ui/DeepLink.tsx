ee8eba625204ed10318236d8f79fdfdf4b7623ae
kolserdav <serega12101983@gmail.com>
2023-07-14 13:53:47 +0700

    try check deep link

--- packages/app/components/ui/DeepLink.tsx
+++ packages/app/components/ui/DeepLink.tsx
@@ -1,11 +1,12 @@
 import { useRouter } from 'next/router';
-import { useMemo } from 'react';
+import { useEffect, useMemo } from 'react';
 import { ANDROID_APP_NAME, DEEP_LINK_HOST } from '../../utils/constants';
 import s from './DeepLink.module.scss';
 import { isAndroid } from '../../utils/lib';
 
 function DeepLink({ children }: { children: string }) {
-  const { pathname } = useRouter();
+  const router = useRouter();
+  const { pathname } = router;
   const search = useMemo(() => {
     if (typeof window !== 'undefined') {
       return window.location.search;
@@ -13,10 +14,31 @@ function DeepLink({ children }: { children: string }) {
     return null;
   }, []);
 
+  const link = `${ANDROID_APP_NAME}://${DEEP_LINK_HOST}${pathname}${search}`;
+
+  /**
+   * Check link
+   */
+  const clickDeepLink = () => {
+    try {
+      fetch(link)
+        .then((r) => {
+          console.log(0, r.status);
+        })
+        .catch((e) => {
+          console.error(1, e);
+        });
+    } catch (e) {
+      alert(1);
+    }
+  };
+
   return (
     <div className={s.wrapper}>
       {isAndroid() && typeof androidCommon === 'undefined' && (
-        <a href={`${ANDROID_APP_NAME}://${DEEP_LINK_HOST}${pathname}${search}`}>{children}</a>
+        <button type="button" onClick={clickDeepLink}>
+          {children}
+        </button>
       )}
     </div>
   );
