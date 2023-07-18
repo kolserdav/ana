1b08ba92a3cb17e6576d5281ac53ede8fc7c43cd
kolserdav <serega12101983@gmail.com>
2023-07-18 15:40:28 +0700

    create the navigation draft

--- data/pages.sql
+++ data/pages.sql
@@ -4,38 +4,45 @@ INSERT INTO public."Page" VALUES (2, 'contacts', 'title', 'Resource owner inform
 INSERT INTO public."Page" VALUES (3, 'contacts', 'content', '<p>контакты</p>', 'ru') ;
 INSERT INTO public."Page" VALUES (4, 'contacts', 'content', '<p>contacts</p>', 'en') ;
 
-INSERT INTO public."Page" VALUES (25, 'donate', 'title', 'Пожертвование', 'ru') ;
-INSERT INTO public."Page" VALUES (26, 'donate', 'title', 'Donation', 'en') ;
-INSERT INTO public."Page" VALUES (27, 'donate', 'description', 'Помогите проекту', 'ru') ;
-INSERT INTO public."Page" VALUES (28, 'donate', 'description', 'Help the project', 'en') ;
-INSERT INTO public."Page" VALUES (29, 'donate', 'content', '', 'ru') ;
-INSERT INTO public."Page" VALUES (30, 'donate', 'content', '', 'en') ;
+INSERT INTO public."Page" VALUES (5, 'donate', 'title', 'Пожертвование', 'ru') ;
+INSERT INTO public."Page" VALUES (6, 'donate', 'title', 'Donation', 'en') ;
+INSERT INTO public."Page" VALUES (7, 'donate', 'description', 'Помогите проекту', 'ru') ;
+INSERT INTO public."Page" VALUES (8, 'donate', 'description', 'Help the project', 'en') ;
+INSERT INTO public."Page" VALUES (9, 'donate', 'content', '', 'ru') ;
+INSERT INTO public."Page" VALUES (10, 'donate', 'content', '', 'en') ;
 
-INSERT INTO public."Page" VALUES (5, 'policy', 'content', '<p>Политика конфиденциальности</p>', 'ru') ;
-INSERT INTO public."Page" VALUES (6, 'policy', 'title', 'Политика конфиденциальности', 'ru') ;
-INSERT INTO public."Page" VALUES (7, 'policy', 'title', 'Privacy Policy', 'en') ;
-INSERT INTO public."Page" VALUES (8, 'policy', 'content', '<p>Privacy Policy</p>', 'en') ;
+INSERT INTO public."Page" VALUES (11, 'policy', 'content', '<p>Политика конфиденциальности</p>', 'ru') ;
+INSERT INTO public."Page" VALUES (12, 'policy', 'title', 'Политика конфиденциальности', 'ru') ;
+INSERT INTO public."Page" VALUES (13, 'policy', 'title', 'Privacy Policy', 'en') ;
+INSERT INTO public."Page" VALUES (14, 'policy', 'content', '<p>Privacy Policy</p>', 'en') ;
 
-INSERT INTO public."Page" VALUES (9, 'rules', 'title', 'Terms of Use
+INSERT INTO public."Page" VALUES (15, 'rules', 'title', 'Terms of Use
     Tes
 ', 'en') ;
-INSERT INTO public."Page" VALUES (10, 'rules', 'title', 'Правила использования', 'ru') ;
-INSERT INTO public."Page" VALUES (11, 'rules', 'content', '<p>Terms of use</p>', 'en') ;
-INSERT INTO public."Page" VALUES (12, 'rules', 'content', '<p>правила использования</p>', 'ru') ;
+INSERT INTO public."Page" VALUES (16, 'rules', 'title', 'Правила использования', 'ru') ;
+INSERT INTO public."Page" VALUES (17, 'rules', 'content', '<p>Terms of use</p>', 'en') ;
+INSERT INTO public."Page" VALUES (18, 'rules', 'content', '<p>правила использования</p>', 'ru') ;
 
-INSERT INTO public."Page" VALUES (13, 'about', 'title', 'О программе и о сервисе', 'ru') ;
-INSERT INTO public."Page" VALUES (14, 'about', 'title', 'About the program and service', 'en') ;
-INSERT INTO public."Page" VALUES (23, 'about', 'description', 'Подробнее о программе и о сервисе', 'ru') ;
-INSERT INTO public."Page" VALUES (24, 'about', 'title', 'More about the program and service', 'en') ;
+INSERT INTO public."Page" VALUES (19, 'about', 'title', 'О программе и о сервисе', 'ru') ;
+INSERT INTO public."Page" VALUES (20, 'about', 'title', 'About the program and service', 'en') ;
+INSERT INTO public."Page" VALUES (21, 'about', 'description', 'Подробнее о программе и о сервисе', 'ru') ;
+INSERT INTO public."Page" VALUES (22, 'about', 'title', 'More about the program and service', 'en') ;
 
-INSERT INTO public."Page" VALUES (15, 'translate', 'title', 'Составление предложений', 'ru') ; 
-INSERT INTO public."Page" VALUES (16, 'translate', 'title', 'Making sentences', 'en') ;
-INSERT INTO public."Page" VALUES (17, 'translate', 'description', 'Приложение помогающее изучать иностранный язык. 
+INSERT INTO public."Page" VALUES (23, 'aboutTranslate', 'title', 'О модуле переводчика', 'ru') ;
+INSERT INTO public."Page" VALUES (24, 'aboutTranslate', 'title', 'About the translate engine', 'en') ;
+INSERT INTO public."Page" VALUES (25, 'aboutTranslate', 'description', 'В данном приложении используется самостоятельный модуль для перевода', 'ru') ;
+INSERT INTO public."Page" VALUES (26, 'aboutTranslate', 'description', 'This application uses an stand-alone translation module', 'en') ;
+INSERT INTO public."Page" VALUES (27, 'aboutTranslate', 'content', 'Подробнее о движке переводчика', 'ru') ;
+INSERT INTO public."Page" VALUES (28, 'aboutTranslate', 'content', 'More about the translate engine', 'en') ;
+
+INSERT INTO public."Page" VALUES (29, 'translate', 'title', 'Составление предложений', 'ru') ; 
+INSERT INTO public."Page" VALUES (30, 'translate', 'title', 'Making sentences', 'en') ;
+INSERT INTO public."Page" VALUES (31, 'translate', 'description', 'Приложение помогающее изучать иностранный язык. 
 Составляйте приложения на языке, который вы изучаете, а алгоритм перевода поправит вас в случае ошибки.', 'ru') ; 
-INSERT INTO public."Page" VALUES (18, 'translate', 'description', 'An application that helps you learn a foreign language. 
+INSERT INTO public."Page" VALUES (32, 'translate', 'description', 'An application that helps you learn a foreign language. 
 Compose applications in the language you are learning and the translation algorithm will correct you if you make a mistake', 'en') ;
-INSERT INTO public."Page" VALUES (19, 'translate', 'keywords', 'блокнот,переводчик,писать предложения,изучать язык', 'ru');
-INSERT INTO public."Page" VALUES (20, 'translate', 'keywords', 'notepad, translator, write sentences, learn language', 'en') ;
+INSERT INTO public."Page" VALUES (33, 'translate', 'keywords', 'блокнот,переводчик,писать предложения,изучать язык', 'ru');
+INSERT INTO public."Page" VALUES (34, 'translate', 'keywords', 'notepad, translator, write sentences, learn language', 'en') ;
 
-INSERT INTO public."Page" VALUES (21, 'my', 'title', 'Мои тексты', 'ru') ;
-INSERT INTO public."Page" VALUES (22, 'my', 'title', 'My texts', 'en') ;
+INSERT INTO public."Page" VALUES (35, 'my', 'title', 'Мои тексты', 'ru') ;
+INSERT INTO public."Page" VALUES (36, 'my', 'title', 'My texts', 'en') ;
